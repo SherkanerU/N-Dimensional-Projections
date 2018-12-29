a0edfb326ae2d7912c3cb5b919e8f7f4510e9187
SherkanerU <42780965+SherkanerU@users.noreply.github.com>
2018-12-29 17:19:39 -0500

    Update PointsWithSpace.java

--- Code/PointsWithSpace.java
+++ Code/PointsWithSpace.java
@@ -31,6 +31,56 @@ public class PointsWithSpace
 	{
 		//first we need to turn the points of the graph
 		//into vectors for projection
-		Vector[] toProject = new Vector[points.]
+		Vector[] toProject = new Vector[points.vertices().length];
+
+		//turn the points in the graph into vectors because
+		//i didnt think out the point graph well...
+		for (int i = 0; i < toProject.length; i++)
+		{
+			toProject[i] = new Vector(points.vertices()[i].cords());
+		}
+
+		//Point[] projections = new Point[toProject.length];
+
+		for(int i = 0; i < projections.length; i++)
+		{
+			double[] projCords = space.projCords(toProject[i])
+			Point point = (Point) space.vectorCords(projCords);
+
+			points.setVertex(i, point);
+		}
+	}
+
+	//reduce the space by 1 dimension of thej 
+	//type specified
+	public void reduceSpace(String type)
+	{
+		space = space.reducedDimension(type);
+	}
+
+	/*****************************************************
+							Getters
+	*****************************************************/
+	public int dimension()
+	{
+		int dim = space.dimensionOfAmbientSpace();
+		if (dim != points.vertices()[].getDimension())
+		{
+			throw new IllegalStateException("space and point graph not of same ambient dimension");
+		}
+
+		return dim;
+	}
+	public int spaceDimension()
+	{
+		return space.dimension();
+	}
+	public PointGraph points()
+	{
+		return points;
+	}
+	public VectorSpace space()
+	{
+		return space;
 	}
 }
